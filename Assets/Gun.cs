21d37640fb461abe1bc45ea047399dc76d3a9a86
jaymaxz <jagathmax2@gamil.com>
2018-05-09 11:37:35 +0530

    Initial commit

---
+++ Assets/Gun.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gun : MonoBehaviour {
+
+    public GameObject bulletPrefab;
+    public Transform spawnObject;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetMouseButtonDown(0)){
+            GameObject go = Instantiate(bulletPrefab, spawnObject.position, spawnObject.rotation) as GameObject;
+            go.GetComponent<Rigidbody>().AddForce(transform.forward * 30, ForceMode.VelocityChange);
+        }
+	    
+	}
+}
